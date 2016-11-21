773c1e2a8c94a5a756f5d0f4c1b3a4fb918fb37a
Jun Chen <junc76@gmail.com>
2016-11-21 18:24:36 +0800

    Add a new test: vector diff

---
+++ test/vector_diff_test.cpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "gtest/gtest.h"
+#include <vector>
+using namespace std;
+
+void diff(const vector<int>& v1, const vector<int>& v2, vector<int>& added, vector<int>& removed){
+
+}
+
+TEST(vector_diff_test, add_and_remove_elements){
+    std::vector<int> v1 = {1, 2, 3, 4};
+    std::vector<int> v2 = {2, 4, 5};
+    std::vector<int> added, removed;
+    diff(v1, v2, added, removed);
+    EXPECT_EQ(1, added.size());
+    EXPECT_EQ(2, removed.size());
+}
