1fb8f49676014a9c922249d50df191a833da50e0
Jun Chen <junc76@gmail.com>
2017-02-22 14:55:33 +0800

    Verify a crash when push_back a nullptr

---
+++ test/push_back_crash.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <memory>
+using namespace std;
+typedef std::shared_ptr<int> IntPtr;
+typedef std::shared_ptr<vector<IntPtr> > IntVtrPtr;
+
+TEST(push_back_crash, example) {
+    IntVtrPtr vPtr1 = make_shared<vector<IntPtr> >(vector<IntPtr>());
+    IntVtrPtr vPtr2 = make_shared<vector<IntPtr> >(vector<IntPtr>());
+    vPtr2->push_back(vPtr1->front());
+}
