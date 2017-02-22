033398af811fd806679ee11d12588ed06a179ea3
Jun Chen <junc76@gmail.com>
2017-02-22 15:50:39 +0800

    Make the push_back example more clean

--- test/push_back_crash.cpp
+++ test/push_back_crash.cpp
@@ -6,8 +6,8 @@ typedef std::shared_ptr<int> IntPtr;
 typedef std::shared_ptr<vector<IntPtr> > IntVtrPtr;
 
 TEST(push_back_crash, example) {
-    IntVtrPtr vPtr1 = make_shared<vector<IntPtr> >(vector<IntPtr>());
-    IntVtrPtr vPtr2 = make_shared<vector<IntPtr> >(vector<IntPtr>());
+    IntVtrPtr vPtr1 = make_shared<vector<IntPtr>>();
+    IntVtrPtr vPtr2 = make_shared<vector<IntPtr>>();
 
     ASSERT_DEATH(vPtr2->push_back(vPtr1->front()), "");
 }
